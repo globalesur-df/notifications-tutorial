dd7e25b24c8c1fe23db4bc8d4d6267cfb0a3805f
Daniel Alejandro Fernández <83842237+globalesur-df@users.noreply.github.com>
2024-11-15 21:51:34 -0300

    first backup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,86 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, Platform, SafeAreaView, StatusBar } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useNotification } from '@/context/NotificationContext';
+// import DOMCoolCode from '@/components/DOMCoolCode';
+import * as Updates from 'expo-updates';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function HomeScreen() {
+  const { notification, expoPushToken, error } = useNotification();
+  const { currentlyRunning, isUpdateAvailable, isUpdatePending } =
+    Updates.useUpdates();
 
-export default function TabOneScreen() {
+  const [dummyState, setDummyState] = useState(0);
+
+  if (error) {
+    return <ThemedText>Error: {error.message}</ThemedText>;
+  }
+
+  useEffect(() => {
+    if (isUpdatePending) {
+      // Update has successfully downloaded; apply it now
+      // Updates.reloadAsync();
+      // setDummyState(dummyState + 1);
+      // Alert.alert("Update downloaded and applied");
+
+      dummyFunction();
+    }
+  }, [isUpdatePending]);
+
+  const dummyFunction = async () => {
+    try {
+      await Updates.reloadAsync();
+    } catch (e) {
+      Alert.alert('Error');
+    }
+
+    // UNCOMMENT TO REPRODUCE EAS UPDATE ERROR
+    // } finally {
+    //   setDummyState(dummyState + 1);
+    //   console.log("dummyFunction");
+    // }
+  };
+
+  // If true, we show the button to download and run the update
+  const showDownloadButton = isUpdateAvailable;
+
+  // Show whether or not we are running embedded code or an update
+  const runTypeMessage = currentlyRunning.isEmbeddedLaunch
+    ? 'This app is running from built-in code'
+    : 'This app is running an update';
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <ThemedView
+      style={{
+        flex: 1,
+        padding: 10,
+        paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 10,
+      }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <ThemedText type="subtitle">Updates Demo 5</ThemedText>
+        <ThemedText>{runTypeMessage}</ThemedText>
+        <Button
+          onPress={() => Updates.checkForUpdateAsync()}
+          title="Check manually for updates"
+        />
+        {showDownloadButton ? (
+          <Button
+            onPress={() => Updates.fetchUpdateAsync()}
+            title="Download and run update"
+          />
+        ) : null}
+        <ThemedText type="subtitle" style={{ color: 'red' }}>
+          Your push token:
+        </ThemedText>
+        <ThemedText>{expoPushToken}</ThemedText>
+        <ThemedText type="subtitle">Latest notification:</ThemedText>
+        <ThemedText>{notification?.request.content.title}</ThemedText>
+        <ThemedText>
+          {JSON.stringify(notification?.request.content.data, null, 2)}
+        </ThemedText>
+      </SafeAreaView>
+    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import * as Notifications from 'expo-notifications';
+
 import { useColorScheme } from '@/components/useColorScheme';
+import { NotificationProvider } from '@/context/NotificationContext';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -49,11 +64,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <NotificationProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </ThemeProvider>
+    </NotificationProvider>
   );
 }
